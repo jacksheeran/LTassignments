9dd6ae45207c4775fd59f84bd216b0a08c0c0499
Jack Sheeran <jacksheeran@hotmail.co.uk>
2019-04-11 18:26:36 +0100

    Update text-file-to-IPA.py

--- text-file-to-IPA.py
+++ text-file-to-IPA.py
@@ -2,23 +2,32 @@ import pronouncing
 
 import string
 
-import arpabetmodule #imported from costum module created in 'arpabetmodule.py'
+import arpabetmodule #imported from costum module 'arpabetmodule.py', converts APRAbet to IPA
 
 def ipa(text):
 """converts text input to IPA"""
+
     text = text.lower()
     textipa = ''
+    
     for word in text.split():
-        wordx = pronouncing.phones_for_word(word.rstrip(',.-/()!?¿¡$%&#@:;'))
+        
+        wordx = pronouncing.phones_for_word(word.rstrip(',.-/()!?¿¡$%&#@:;')) 
+        
         if wordx == []:
+            
             if textipa == '':
                 textipa = textipa + (len(word)*'?')
+                
             else:
                 textipa = textipa + ' ' + (len(word)*'?')
+                
         else:
-            wordy = arpabet.ipa_word(wordx[0])
+            wordy = arpabetmodule.ipa_word(wordx[0])
+            
             if textipa == '':
                 textipa = textipa + wordy
+                
             else:
                 textipa = textipa + ' ' + wordy
     return textipa
@@ -27,6 +36,9 @@ def ipafile(filename):
 """outputs a file with text from input file converted to IPA"""
 
     with open('withipa_' + filename, 'w') as nf:
+        
         with open(filename) as f:
+            
             for line in f:
+                
                 print(ipa(line), file=nf)
